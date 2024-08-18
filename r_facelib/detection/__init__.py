915652bfbf8f6b8f5f955ceab4bbf33a68db93c3
Mert Güvençli <guvenclimert@gmail.com>
2024-08-18 13:02:31 +0300

    Add COMFYUI_MODELS_PATH

--- r_facelib/detection/__init__.py
+++ r_facelib/detection/__init__.py
@@ -33,7 +33,7 @@ def init_retinaface_model(model_name, half=False, device='cuda'):
     else:
         raise NotImplementedError(f'{model_name} is not implemented.')
 
-    model_path = load_file_from_url(url=model_url, model_dir='../../models/facedetection', progress=True, file_name=None)
+    model_path = load_file_from_url(url=model_url, model_dir=os.path.join(os.environ['COMFYUI_MODELS_PATH'], 'facedetection'), progress=True, file_name=None)
     load_net = torch.load(model_path, map_location=lambda storage, loc: storage)
     # remove unnecessary 'module.'
     for k, v in deepcopy(load_net).items():
@@ -58,7 +58,7 @@ def init_yolov5face_model(model_name, device='cuda'):
     else:
         raise NotImplementedError(f'{model_name} is not implemented.')
     
-    model_path = load_file_from_url(url=model_url, model_dir='../../models/facedetection', progress=True, file_name=None)
+    model_path = load_file_from_url(url=model_url, model_dir=os.path.join(os.environ['COMFYUI_MODELS_PATH'], 'facedetection'), progress=True, file_name=None)
     load_net = torch.load(model_path, map_location=lambda storage, loc: storage)
     model.detector.load_state_dict(load_net, strict=True)
     model.detector.eval()
--- r_facelib/detection/yolov5face/utils/extract_ckpt.py
+++ r_facelib/detection/yolov5face/utils/extract_ckpt.py
@@ -1,5 +1,6 @@
 import torch
 import sys
+import os
 sys.path.insert(0,'./facelib/detection/yolov5face')
 model = torch.load('facelib/detection/yolov5face/yolov5n-face.pt', map_location='cpu')['model']
-torch.save(model.state_dict(),'../../models/facedetection')
+torch.save(model.state_dict(), os.path.join(os.environ['COMFYUI_MODELS_PATH'], 'facedetection'))
